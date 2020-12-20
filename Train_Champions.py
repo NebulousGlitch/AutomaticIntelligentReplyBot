f5fa2b80c9743faf0ef887abc4360d7201af66d1
NebulousGlitch <65990273+NebulousGlitch@users.noreply.github.com>
2020-12-19 20:07:31 -0500

    Add files via upload
    Should only use occassionally (once every week). Takes data from posts containing every champion.

---
+++ Train_Champions.py
@@ -0,0 +1,70 @@
+from chatterbot import ChatBot
+from chatterbot.trainers import ChatterBotCorpusTrainer
+from chatterbot.trainers import ListTrainer
+import sys
+import tweepy
+import time
+import re
+
+## CHAMPION TRAIN
+## FOR TRAINING CONVOS RELATED TO SPECIFIC CHAMPIONS
+
+CONSUMER_KEY = "PPiJCrYGJsfQgnDbhpEEU7QsQ"
+CONSUMER_SECRET = "IzHjFR2FmKFMCFVcnc0E5ItPaQZCQ7RftbVwKO3jFCAaYhibtc"
+ACCESS_KEY = "1079497682299863042-zkmR16RbeSAWh8v1LHqduQNyJ0ouyn"
+ACCESS_SECRET = "KVFw5BefpIWF8h5ES4q6NRx1gZhTrwcQX7dGo1b4p1Npb"
+
+auth = tweepy.OAuthHandler(CONSUMER_KEY, CONSUMER_SECRET)
+auth.set_access_token(ACCESS_KEY, ACCESS_SECRET)
+api = tweepy.API(auth, wait_on_rate_limit=True)
+
+search_words = "Aatrox,Ahri,Akali,Alistar,Amumu,Anivia,Annie,Aphelios,Ashe,Aurelion Sol,Azir,Bard,Blitzcrank,Brand,Braum,Caitlyn,Camille,Cassiopeia,Cho'Gath,Corki,Darius,Diana,Dr. Mundo,Draven,Ekko,Elise,Evelynn,Ezreal,Fiddlesticks,Fiora,Fizz,Galio,Gangplank,Garen,Gnar,Gragas,Graves,Hecarim,Heimerdinger,Illaoi,Irelia,Ivern,Janna,Jarvan IV,Jax,Jayce,Jhin,Jinx,Kai'Sa,Kalista,Karma,Karthus,Kassadin,Katarina,Kayle,Kayn,Kennen,Kha'Zix,Kindred,Kled,Kog'Maw,LeBlanc,Lee Sin,Leona,Lillia,Lissandra,Lucian,Lulu,Lux,Malphite,Malzahar,Maokai,Master Yi,Miss Fortune,Mordekaiser,Morgana,Nami,Nasus,Nautilus,Neeko,Nidalee,Nocturne,Nunu and Willump,Olaf,Orianna,Ornn,Pantheon,Poppy,Pyke,Qiyana,Quinn,Rakan,Rammus,Rek'Sai,Rell,Renekton,Rengar,Riven,Rumble,Ryze,Samira,Sejuani,Senna,Seraphine,Sett,Shaco,Shen,Shyvana,Singed,Sion,Sivir,Skarner,Sona,Soraka,Swain,Sylas,Syndra,Tahm Kench,Taliyah,Talon,Taric,Teemo,Thresh,Tristana,Trundle,Tryndamere,Twisted Fate,Twitch,Udyr,Urgot,Varus,Vayne,Veigar,Vel'Koz,Vi,Viktor,Vladimir,Volibear,Warwick,Wukong,Xayah,Xerath,Xin Zhao,Yasuo,Yone,Yorick,Yuumi,Zac,Zed,Ziggs,Zilean,Zoe,Zyra"
+
+Query = search_words.split(",")
+
+non_bmp_map = dict.fromkeys(range(0x10000, sys.maxunicode + 1), 0xfffd)
+
+def find_twitter_conversation():
+    list_of_conversations = []
+    f = open('stored_tweets.txt','r',encoding='utf-8')
+    stored_tweets = f.read()
+    for eachQuery in Query:
+        thread_links = api.search(q=eachQuery+' League of Legends',lang='en',
+                                  result_type='recent',count=50,
+                                  tweet_mode='extended')
+        for eachLink in thread_links:
+            try:
+                x = eachLink.retweeted_status
+            except:
+                if eachLink.full_text in list_of_conversations:
+                    break
+                else:
+                    text = eachLink.full_text
+                    result = re.sub(r"http\S+", "", text) #taken from stackexchange
+                    even_cleaner = " ".join(filter(
+                        lambda x:x[0]!='@', result.split())) #taken from stackexchange
+                    if even_cleaner in stored_tweets:
+                        break
+                    else:
+                        list_of_conversations.append(even_cleaner)
+        print('Found tweets for ' + eachQuery)
+        time.sleep(5)
+    f.close()
+    f = open('stored_tweets.txt','a',encoding='utf-8')
+    for eachString in list_of_conversations:
+        f.write(eachString + ' ')         
+    return list_of_conversations
+
+def train_database():
+    chatbot = ChatBot('Ron Obvious')
+
+    trainer = ListTrainer(chatbot)
+
+    trainer.train(
+        find_twitter_conversation()
+        )
+    print('\nsuccessfully trained!\n')
+
+while True:
+    train_database()
+    time.sleep(1800)
